5c4106b5f8686d22d38c5a6ad1fb41c2c2df819f
BabyblueSheep <47078394+BabyblueSheep@users.noreply.github.com>
2026-04-10 12:40:28 +0300

    Initial commit

---
+++ src/Retro2DGame/Program.cs
@@ -0,0 +1,31 @@
+﻿using MoonWorks;
+
+namespace Retro2DGame;
+
+internal sealed class Program
+{
+    static void Main(string[] args)
+    {
+        AppInfo appInfo = new
+        (
+            OrganizationName: "babybluesheep",
+            ApplicationName: "Retro 2D Game"
+        );
+
+        WindowCreateInfo windowCreateInfo = new
+        (
+            windowTitle: "Bullet Game Prototype",
+            windowWidth: 640,
+            windowHeight: 480,
+            screenMode: ScreenMode.Windowed,
+            systemResizable: true,
+            startMaximized: false,
+            highDPI: false
+        );
+
+        FramePacingSettings framePacingSettings = FramePacingSettings.CreateCapped(60, 6);
+
+        var game = new BulletGame(appInfo, windowCreateInfo, framePacingSettings);
+        game.Run();
+    }
+}
